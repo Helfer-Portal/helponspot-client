e4c643ce62cd9f777222fa1afbdd407c2e5b8d51
Yannick Streicher <yannick.streicher@uni.kn>
2020-04-02 09:27:10 +0200

    Created Button Components for each gradient in Layout

--- src/components/ButtonOrange.jsx
+++ src/components/ButtonOrange.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Button extends Component {
   render() {
     return (
-      <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg">
+      <button class="mr-auto lg:mx-0 hover:underline orange-gradient text-white font-bold font-inter rounded-full my-6 py-4 px-8 shadow-lg">
             {this.props.children}
       </button>
     )
--- src/components/ButtonWithLink.jsx
+++ src/components/ButtonWithLink.jsx
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import { Link } from "react-router-dom";
-import Button from "./Button"
+import ButtonOrange from "./ButtonOrange"
 
 export default class ButtonWithLink extends Component {
   render() {
     return (
         <Link to={this.props.link}>
-          <Button children={this.props.children}/>
+          <ButtonOrange children={this.props.children}/>
         </Link>
     )
   }
