ddda3b364c07131eff4c5e872ca52541c8f5e992
Yannick Streicher <yannick.streicher@uni.kn>
2020-04-03 11:25:03 +0200

    Implemented mock features for counter and date picker

---
+++ src/features/org-register/components/counter.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Circle from "../../../components/Circle";
+
+export default function Counter(props) {
+  const [count, setCount] = props.countState;
+
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const infty = () => {
+      setCount(-1);
+  }
+
+  return (
+    <div className="flex flex-row">
+      <div className="flex">
+        <div onClick={decrease}>
+          <Circle image="/minus.png" />
+        </div>
+        <div class="number">{count < 0 ? "alle" : count}</div>
+        <div onClick={increase}>
+          <Circle image="/plus.png" />
+        </div>
+        <button class="unlimited" onClick={infty}>unbegrenzt</button>
+      </div>
+    </div>
+  );
+}
--- src/features/org-register/newAnzeige.jsx
+++ src/features/org-register/newAnzeige.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import QuestionWithLabel from "../../components/QuestionWithLabel";
-import Circle from "../../components/Circle";
+
 import "./register1.css";
 import FullHeightLayout from "./components/full-height-layout";
 import Competences from "./components/competences";
 import { ReqContext } from "../../context/mock-requests";
 import { Redirect } from "react-router-dom";
+import Counter from "./components/counter";
 
 export default function NewAnzeige() {
   let [redirect, setRedirect] = React.useState(false);
@@ -14,6 +15,8 @@ export default function NewAnzeige() {
 
   let [title, setTitle] = React.useState("");
   let [date, setDate] = React.useState("");
+  let [dateDiff, setDateDiff] = React.useState(0);
+  let [count, setCount] = React.useState(0);
 
   const updateTitle = e => {
     setTitle(e.target.value);
@@ -21,19 +24,29 @@ export default function NewAnzeige() {
 
   const updateDate = e => {
     setDate(e.target.value);
+    let diff = datediff(Date.now(), new Date(e.target.value));
+    setDateDiff(diff);
   };
+
+  /** thank you Stackoverflow https://stackoverflow.com/questions/542938/how-do-i-get-the-number-of-days-between-two-dates-in-javascript */
+  function datediff(first, second) {
+    // Take the difference between the dates and divide by milliseconds per day.
+    // Round to nearest whole number to deal with DST.
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
+  }
 
   const addData = e => {
     e.preventDefault();
     setData(prevData => [
       {
         title: title,
-        timeLast: Math.floor(Math.random() * 10) + "2 Tage",
-        reqHelpers: Math.floor(Math.random() * 10),
-        confirmed: Math.floor(Math.random() * 10),
+        timeLast: dateDiff ? dateDiff + " Tage" : "5 Tage",
+        reqHelpers: count,
+        confirmed: Math.floor(Math.random() * count),
         denied: 0,
         open: 0
-      }, ...prevData
+      },
+      ...prevData
     ]);
     setRedirect(true);
   };
@@ -58,18 +71,13 @@ export default function NewAnzeige() {
       </div>
 
       <div className="w-full flex" class="container-helper-numbers">
-        <div>
-          <Circle image="/minus.png" />
-          <div class="number"> 5 </div>
-          <Circle image="/plus.png" />
-        </div>
-          <button class="unlimited">unbegrenzt</button>
+        <Counter countState={[count, setCount]} />
       </div>
 
       <div className="w-full flex flex-col">
         <div className="mb-3 text-figmaDescription font-inter">Zeitraum</div>
         <div>
-          <input type="date"></input>
+          <input type="date" value={date} onChange={updateDate}></input>
         </div>
       </div>
 
