c4181316acda5a90fc96021cab4756a7152520f4
Yannick Streicher <yannick.streicher@uni.kn>
2020-04-03 11:53:05 +0200

    set max width

--- src/features/org-register/components/full-height-layout-no-footer.jsx
+++ src/features/org-register/components/full-height-layout-no-footer.jsx
@@ -5,7 +5,7 @@ import Footer from "../../landing-page/components/footer";
 /** Layout for the register desktop story */
 export default function FullHeightLayoutNoFooter(props) {
   return (
-    <div className="flex flex-col m-auto w-full h-full">
+    <div className="flex flex-col m-auto w-full max-w-screen-lg h-full">
       <div style={{height: '100vh'}} class="pt-8 align-start justify-start pl-20 h-2/3 container-form ">
         {props.children}
       </div>
