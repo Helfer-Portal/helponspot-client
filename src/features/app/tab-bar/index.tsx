47f30a228234daae4e42d78cddb2a3dc0242dd18
Yannick Streicher <info@yannickstreicher.org>
2020-04-07 14:35:47 +0200

    Created first Tab Menu Bar version
    Therefore Adjusted the height of the dummy iphone screen and created a navigation Component.

---
+++ src/features/app/tab-bar/index.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+interface TabProps {
+  to: string;
+  label: string;
+}
+
+/** single Tab */
+const Tab = ({ to, label }: TabProps) => {
+  return (
+    <div style={{ flex: 1 }} className="flex  justify-center items-center">
+      <div>
+        <NavLink to={to} activeClassName="tab-link bg-black">
+          <div
+            style={{ color: "#F58576" }}
+            className="text-sm font-inter text-center"
+          >
+            <div style={{ display: "inline-flex" }} className="">
+              {/* TODO: include Icons */}
+              *icon
+            </div>
+            <div className="hidden ml-2" id="tab-label">
+              {label}
+            </div>
+          </div>
+        </NavLink>
+      </div>
+    </div>
+  );
+};
+
+const TabBar = () => {
+  return (
+    <div
+      style={{ position: "absolute", height: 55, bottom: 20, width: 365 }}
+      className="bg-white flex border-t-2"
+    >
+      <Tab to={"dashboard"} label={"Dashboard"} />
+      <Tab to={"request"} label={"Anzeige"} />
+      <Tab to={"map"} label={"Karte"} />
+      <Tab to={"profile"} label={"Profil"} />
+    </div>
+  );
+};
+
+export default TabBar;
