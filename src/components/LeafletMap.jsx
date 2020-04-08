a6fda8db0cecd63883ae380029990c5c1983aa0c
CosmicGans <mahas113@uni-duesseldorf.de>
2020-04-08 20:06:54 +0200

    replace GeoJsonTag

--- src/components/LeafletMap.jsx
+++ src/components/LeafletMap.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./leaflet-map.css";
-import { Map, Marker, TileLayer, GeoJSON } from "react-leaflet";
+import { Map, Marker, TileLayer, GeoJSON, Popup, FeatureGroup } from "react-leaflet";
 import L from "leaflet";
 import helperIconUrl from "../assets/helfer.png";
 import organisationIconUrl from "../assets/organisationen.png";
 import iconShadowUrl from "../assets/shadow.png";
+import { Link } from "react-router-dom";
 
 /* This component relies on leaflet.css, which
 currently is imported in a link tag in index.html */
@@ -41,6 +42,8 @@ export const icons = {
 class LeafletMap extends React.Component {
   constructor(props) {
     super(props);
+    this.data = this.props.geojson.default.features;
+
     this.state = {
       viewport: {
         center: this.props.location,
@@ -74,23 +77,23 @@ class LeafletMap extends React.Component {
 
   }
 
-  createPopup(feature, layer) {
-    /* The method bindPopup only takes strings
-    but the string can contain html, which will
-    then be parsed and rendered */
-    const name = feature.properties.name;
+  createMarkerWithPopup(feature) {
+
     const id = feature.properties.id;
-    var popupContent = "";
-    popupContent += "<Popup>";
-    //include link to user
-    popupContent += `<h2><b><a href='/app/organisation/user/${id}'>${name}</a></b></h2>`;
-    popupContent += "<p><b>Bringt mit:</b></p>";
-    popupContent += "<ul>";
-    feature.properties.qualities.forEach((quality) => {
-      popupContent += "<li>" + quality + "</li>";
-    });
-    popupContent += "</ul></Popup>";
-    layer.bindPopup(popupContent);
+    const name = feature.properties.name;
+
+    return (
+        <Marker onClick={this.zoomToMarker(feature)} onPopupclose={this.zoomBack.bind(this)}
+         icon={icons[this.props.icon]} position= {{lat: feature.geometry.coordinates[1], lng: feature.geometry.coordinates[0]}}>
+          <Popup>
+            <Link to={`/app/organisation/user/${id}`}> {name}</Link>
+            <p><b>Bringt mit:</b></p>"
+            {feature.properties.qualities.map(quality =>
+                <li>  {quality } </li>)
+            };
+          </Popup>
+        </Marker> )
+
   }
 
 
@@ -115,12 +118,12 @@ class LeafletMap extends React.Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <GeoJSON
-          data={this.props.geojson.default.features}
-          pointToLayer={this.pointToLayer.bind(this)}
-          onEachFeature={this.onEachFeature.bind(this)}
-          viewport={this.state.viewport}
-        />
+
+        {
+          this.data.map( feature => {
+            return this.createMarkerWithPopup(feature)
+          })}
+
       </Map>
     );
   }
