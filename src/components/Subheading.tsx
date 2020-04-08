1b5776da7f91062f4f5fd4b38c9c6861db3b1f02
Yannick Streicher <info@yannickstreicher.org>
2020-04-08 13:10:19 +0200

    Moved Back Button and Subheading

---
+++ src/components/Subheading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const Subheading = ({ children }: { children: string }) => (
+  <div className="uppercase text-sm font-inter text-figmaParagraph p-1">
+    {children}
+  </div>
+);
