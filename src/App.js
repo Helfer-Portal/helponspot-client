6aa92bb5ab993265c2ff0b3d913a43079142ae05
Milad Khanibeik <milad.khanibeik@exxeta.com>
2020-04-17 16:22:27 +0200

    enable cognito federated login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,58 @@
 import React from "react";
-import Amplify from "aws-amplify";
+import Amplify, { Auth } from "aws-amplify";
 import "@aws-amplify/ui/dist/style.css";
-import { withAuthenticator } from "aws-amplify-react";
-import { awsConfig, signUpConfig } from "./aws-exports";
+import { awsConfig } from "./aws-exports";
 import "./App.css";
 import RootRouter from "./router/index.js";
 
 Amplify.configure(awsConfig);
 
-function App() {
+export default function App() {
   document.body.classList.add("gradient");
+  const isAuthenticated = localStorage.getItem("isAuthenticated");
+
+  if (!isAuthenticated || isAuthenticated == "false") {
+    Auth.federatedSignIn().then(() => {
+      localStorage.setItem("isAuthenticated", true);
+    });
+  } else {
+    logCurrentSession();
+  }
+
   return <RootRouter />;
 }
 
-export default withAuthenticator(App, { includeGreetings: true, signUpConfig });
+function logCurrentSession() {
+  try {
+    Auth.currentSession().then((session) => {
+      console.log(session);
+    });
+  } catch (e) {
+    console.log("Error getting currentSession: " + e);
+  }
+
+  try {
+    Auth.currentAuthenticatedUser().then((authenticatedUser) => {
+      console.log(authenticatedUser);
+    });
+  } catch (e) {
+    console.log("Error getting currentAuthenticatedUser: " + e);
+  }
+
+  try {
+    Auth.currentCredentials().then((credentials) => {
+      console.log(credentials);
+    });
+  } catch (e) {
+    console.log("Error getting currentCredentials: " + e);
+  }
+}
+
+// export default withAuthenticator(App, {
+//   includeGreetings: true,
+//   signUpConfig,
+//   federated: {
+//     amazon_client_id: "amzn1.application-oa2-client.6ba1a92ede974d9cb370d87475dcc9ba",
+//     google_client_id: "712951823298-63bvjn7kmibubnc1pae68egovm3rijo8.apps.googleusercontent.com"
+//   }
+// });
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -4,6 +4,13 @@ export const awsConfig = {
     region: "eu-central-1",
     userPoolId: "<your-pool-id>",
     userPoolWebClientId: "<your-pool-client-id>",
+    oauth: {
+      domain: "helponspot.auth.eu-central-1.amazoncognito.com",
+      scope: ["email", "openid"],
+      redirectSignIn: "http://localhost:3000/",
+      redirectSignOut: "http://localhost:3000/",
+      responseType: "code",
+    },
   },
 };
 export const signUpConfig = {
