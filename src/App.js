66fe53fc2bd1c7946cd2b24acdcf22fd13d5162e
Sebastian Boolean <truefalse@posteo.de>
2020-04-20 11:42:12 +0200

    add Hub listener

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import Amplify, { Auth } from "aws-amplify";
+import React, { useEffect, useState } from "react";
+import Amplify, { Auth, Hub } from "aws-amplify";
 import "@aws-amplify/ui/dist/style.css";
 import { awsConfig } from "./aws-exports";
 import "./App.css";
@@ -8,18 +8,55 @@ import RootRouter from "./router/index.js";
 Amplify.configure(awsConfig);
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(
+    // this is only executed once when the App renders
+    () => {
+      Hub.listen("auth", ({ payload: { event, data } }) => {
+        console.log("Hub listen: ", event, data);
+        switch (event) {
+          case "signIn":
+            setUser(data);
+            break;
+          case "signOut":
+            setUser(null);
+            break;
+        }
+      });
+      Auth.currentAuthenticatedUser()
+        .then((user) => setUser(user))
+        .catch(() => console.log("Not signed in"));
+      return () => {
+        Hub.remove("signIn");
+        Hub.remove("signOut");
+      };
+    },
+    []
+  );
+
   document.body.classList.add("gradient");
   const isAuthenticated = localStorage.getItem("isAuthenticated");
 
-  if (!isAuthenticated || isAuthenticated == "false") {
-    Auth.federatedSignIn().then(() => {
-      localStorage.setItem("isAuthenticated", true);
-    });
-  } else {
-    logCurrentSession();
-  }
+  // if (!isAuthenticated || isAuthenticated == "false") {
+  //   Auth.federatedSignIn().then(() => {
+  //     debugger;
+  //     console.log("setting localstorage");
+  //     localStorage.setItem("isAuthenticated", true);
+  //   });
+  // } else {
+  //   logCurrentSession();
+  // }
+  logCurrentSession();
 
-  return <RootRouter />;
+  return (
+    <div>
+      {JSON.stringify(user)}
+      <button onClick={() => Auth.signOut()}>logout</button>
+      <button onClick={() => Auth.federatedSignIn()}>Open Hosted UI</button>
+      <RootRouter />
+    </div>
+  );
 }
 
 function logCurrentSession() {
