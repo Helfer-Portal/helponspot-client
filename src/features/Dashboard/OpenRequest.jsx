ac2258f98e621b39f6a3261bd0bc776d98f9c0a4
CosmicGans <mahas113@uni-duesseldorf.de>
2020-04-20 09:49:09 +0200

    add green ButtonPrimaryGreen.tsx to competences

--- src/features/Dashboard/OpenRequest.jsx
+++ src/features/Dashboard/OpenRequest.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ButtonWithLink from "../../components/ButtonWithLink";
+import { ButtonSecondaryOrange } from "../../components/UiKit";
+import ButtonPrimaryGreen from "../../components/UiKit/ButtonPrimaryGreen";
 
 export default function OpenRequest(props) {
   return (
@@ -18,26 +21,47 @@ export default function OpenRequest(props) {
           {props.reqHelpers < 0 ? "alle" : props.reqHelpers} Teilnehmer
         </p>
       </div>
+      {!props.helper && (
+        <div className="mt-2 flex flex-col">
+          <div className="my-1 flex">
+            <div className="bg-figmaAccept inline-block rounded-lg px-2 py-1">
+              {props.confirmed} angenommen
+            </div>
+          </div>
 
-      <div className="mt-2 flex flex-col">
-        <div className="my-1 flex">
-          <div className="bg-figmaAccept inline-block rounded-lg px-2 py-1">
-            {props.confirmed} angenommen
+          <div className="my-1 flex">
+            <div className="bg-figmaDeny inline-block rounded-lg px-2 py-1">
+              {props.denied} abgelehnt
+            </div>
+          </div>
+
+          <div className="my-1 flex">
+            <div className="bg-figmaOpen inline-block rounded-lg px-2 py-1">
+              {props.open} offen
+            </div>
           </div>
         </div>
-
-        <div className="my-1 flex">
-          <div className="bg-figmaDeny inline-block rounded-lg px-2 py-1">
-            {props.denied} abgelehnt
+      )}
+      {props.helper && (
+        <div style={{ flex: 1 }} className="flex py-4 flex-row">
+          {/*settingPermission to True triggers update */}
+          <div style={{ flex: 1 }} className="pr-2">
+            <ButtonPrimaryGreen
+              children="ja!"
+              link="/app/helfer/createHelper/standort/"
+            />
+          </div>
+          <div style={{ flex: 1 }}>
+            <div className="pl-2">
+              <ButtonSecondaryOrange
+                onClick={() => alert("was machen wir hier?")}
+              >
+                vorerst nicht
+              </ButtonSecondaryOrange>
+            </div>
           </div>
         </div>
-
-        <div className="my-1 flex">
-          <div className="bg-figmaOpen inline-block rounded-lg px-2 py-1">
-            {props.open} offen
-          </div>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
