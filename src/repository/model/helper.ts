6303c4347749cbc771154754f935408cc2885d34
Yannick Streicher <info@yannickstreicher.org>
2020-04-20 15:54:12 +0200

    Updated Data Types in order to match api
    Optional for backwards compatibilty

--- src/repository/model/helper.ts
+++ src/repository/model/helper.ts
@@ -1,7 +1,23 @@
 export interface Helper {
-    id?: number;
-    name: string;
-    email: string;
-    phone: string;
-    postcode: string;
+  id?: number;
+  name: string;
+  email: string;
+  phone: string;
+  postcode: string;
+}
+
+export interface NewHelperPostRequest {
+  lastName: string;
+  firstName: string;
+  email: string;
+  isGPSLocationAllowed: boolean;
+  address: {
+    street: string;
+    houseNumber: string;
+    postalCode: string;
+    city: string;
+    country: string;
+  };
+  qualifications: string[];
+  avatar: string;
 }
--- src/repository/model/helprequest.ts
+++ src/repository/model/helprequest.ts
@@ -1,37 +1,36 @@
-import {Helper} from "./helper"
+import { Helper } from "./helper";
 export interface HelpRequest {
-    id: number;
-    name: string;
-    created_at: string;
-    date_start: string;
-    organisation_id: number;
-    number_helpers:number
-    skills: Skill[];
-    requested_helpers: Helper[];
-    confirmed_helpers: Helper[];
+  id: number;
+  name: string;
+  created_at: string;
+  date_start: string;
+  organisation_id: number;
+  number_helpers: number;
+  skills: Skill[];
+  requested_helpers: Helper[];
+  confirmed_helpers: Helper[];
 }
 
 export interface Skill {
-    id: number;
-    name: string;
+  id: number;
+  name: string;
+  key?: string;
 }
 
 export interface AvailableSkill {
-    skill: Skill;
-    count: number;
+  skill: Skill;
+  count: number;
 }
 
 export interface HelpRequestHelpers {
-    count: number;
-    skills: AvailableSkill[];
+  count: number;
+  skills: AvailableSkill[];
 }
 export interface OrganizationInfo {
-    id:number;
-    name:string;
-    description:string;
-    email:string;
-    phone:string;
-    address:string;
-
-
+  id: number;
+  name: string;
+  description: string;
+  email: string;
+  phone: string;
+  address: string;
 }
